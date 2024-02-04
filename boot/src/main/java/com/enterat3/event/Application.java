b875775f969173122421903a0fe678baebac0b7f
Jose Luis Sanchez Boj <joselsbo@ext.inditex.com>
2024-02-04 16:14:09 +0100

    project maven multimodule with hex architecture and jacoco-report

---
+++ boot/src/main/java/com/enterat3/event/Application.java
@@ -0,0 +1,12 @@
+package com.enterat3.event;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ boot/src/main/java/com/enterat3/event/config/MyClass.java
@@ -0,0 +1,17 @@
+package com.enterat3.event.config;
+
+public class MyClass {
+    
+    
+    public static String myMethod() {
+        return "Hello, World!";
+    }
+    
+    public static Integer myMethod2() {
+        return 42;
+    }
+    
+    public static String myMethod3() {
+        return "Hello, World!";
+    }
+}
